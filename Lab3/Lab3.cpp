0ccbb172cceb570c471f7eec9ef03ca9fa9cda11
Shosetsu <romamarin16@gmail.com>
2020-10-14 16:53:31 +0300

    lab3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -2,39 +2,68 @@
 #include <stdlib.h>
 #include <string>
 
+using namespace std;
+
 struct Banknote {
-    std::string name;
+    string name;
     int denomination;
-    int exchange_rates;
+    int exchangeRates;
 };
+
+void sumArray(int *arr, int n);
+void sumArray(Banknote *banknotes, int n);
 
 int main() {
     int n = 1;
-    std::cout << "Введите количество Банкнот, которые вы хотите добавить\n";
-    std::cin >> n;
-    Banknote banknotes[n];
-    int a[n];
+    cout << "Введите количество Банкнот, которые вы хотите добавить\n";
+    cin >> n;
+    Banknote *banknotes = new Banknote[n];
+    int *int_arr = new int[n];
 
     for (int i = 0; i < n; i++) {
-        std::cout << "Введите название банкноты №" << 1 + i << '\n';
-        std::cin >> banknotes[i].name;
-        std::cout << "Введите номинал для " << banknotes[i].name << '\n';
-        std::cin >> banknotes[i].denomination;
-        std::cout << "Введите курс к рублю для " << banknotes[i].name << '\n';
-        std::cin >> banknotes[i].exchange_rates;
-        a[i] = banknotes[i].denomination * banknotes[i].exchange_rates;
+        cout << "Введите название банкноты №" << 1 + i << endl;
+        cin >> banknotes[i].name;
+        cout << "Введите номинал для " << banknotes[i].name << endl;
+        cin >> banknotes[i].denomination;
+        cout << "Введите курс к рублю для " << banknotes[i].name << endl;
+        cin >> banknotes[i].exchangeRates;
     }
 
     for (int i = 0; i < n; i++) {
-        std::cout << "Банкнота: " << banknotes[i].name << " с номиналом: " << banknotes[i].denomination 
-            << " и курсом к рублю: " << banknotes[i].exchange_rates << '\n';
+        cout << "Банкнота: " << banknotes[i].name << " с номиналом: " << banknotes[i].denomination 
+            << " и курсом к рублю: " << banknotes[i].exchangeRates << endl;
     }
+
+    sumArray(banknotes, n);
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+       int_arr[i] = rand() % 100;
+       cout << "Массив int " << int_arr[i] << "\t";  
+    }
+
+    cout << endl;
+    sumArray(int_arr, n);
     
+    delete[] banknotes;
+    delete[] int_arr;
 }
 
-void summa(int a, int n) {
-    int sum;
-    for (int i = 0; i < n; i++) {
-    
+void sumArray(Banknote *banknotes, int n){
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += banknotes[i].exchangeRates * banknotes[i].denomination;
     }
+    cout << endl << "Сумма банкнот в рублях = " << sum << endl;
+}
+
+void sumArray(int *arr, int n) {
+        double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    cout << endl << "Сумма массива int = " << sum << endl;
 }
