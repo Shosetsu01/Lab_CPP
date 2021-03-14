dd45f2c07a1faef7a6341c10ad00ab299ebe6fda
Shosetsu <romamarin16@gmail.com>
2021-03-14 15:18:16 +0300

    make directory

--- Semestr2/Lab 1/Lab1.cpp
+++ Semestr2/Lab 1/Lab1.cpp
@@ -1,18 +1,43 @@
-#include <chrono>
 #include <iostream>
+#include <chrono>
+#include <random>
+#include <ctime>
+#include <vector>
+
 using namespace std;
 
-int main()
-{
-    int a = 5, b = 10;
- 
-    chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+void sort(double data[], int lenD) {
+  int j = 0;
+  int tmp = 0;
+  for(int i=0; i<lenD; i++){
+    j = i;
+    for(int k = i; k<lenD; k++){
+      if(data[j]>data[k]){
+        j = k;
+      }
+    }
+    tmp = data[i];
+    data[i] = data[j];
+    data[j] = tmp;
+  }
+}
+
+int main(){
+  double N = 1000;
+  vector<double> v(N);
+
+  mt19937 engine(time(0));
+  uniform_real_distribution<double> gen(-1.0, 1.0);
+  for(auto& el: v)
+    el = gen(engine);
+
+  chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+  sort(v, N);
   chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
   chrono::duration<double, nano> nano_diff = end - start;
   chrono::duration<double, micro> micro_diff = end - start;
   chrono::duration<double, milli> milli_diff = end - start;
   chrono::duration<double> sec_diff = end - start;
-    cout << a - b;
   cout << endl;
   cout << "Time: " << nano_diff.count() << " nano sec." << endl;
   cout << "Time: " << micro_diff.count() << " micro sec." << endl;
